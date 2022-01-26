28c9df6927d3f64379b6df9e5b5811add26af57c
Quent <quentin.vincent@efrei.net>
2022-01-26 11:15:06 +0100

    added isPrime command

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -5,5 +5,22 @@ export const utils = {
     year: date.getFullYear(),
     tag(user) {
         return "<@" + user.id + ">"
+    },
+    isPrime(nb):Boolean {
+        if (nb <= 3)
+            return nb > 1
+        else if (nb % 2 == 0 || nb % 3 == 0)
+            return false
+
+        let i = 5
+        while (i * i <= nb) {
+             // console.log("i * i :", i * i)
+             // console.log("nb % i :", nb % i)
+             // console.log("nb % (i + 2) :", nb % (i + 2))
+            if (nb % i == 0 || nb % (i + 2) == 0)
+                return false
+            i = i + 6
+        }
+        return true
     }
 };
